e3e7e9af7f02dcc35a04b07db42bae35b3ede0fd
rafaelrc7 <rafaelrc7@gmail.com>
2021-08-11 08:30:49 -0300

    Added and changed some mappings

--- lua/maps.lua
+++ lua/maps.lua
@@ -5,11 +5,26 @@ local map = vim.api.nvim_set_keymap;
 map("n", "<space>", "", {noremap = true});
 vim.g.mapleader = " ";
 
+-- Terminal
+map("n", "<leader>tt", ":terminal<CR>", {});
+map("n", "<leader>tj", ":bot 10new +terminal | setlocal nobuflisted<CR>", {});
+map("n", "<leader>tl", ":botright vnew +terminal | setlocal nobuflisted<CR>", {});
+map("t", "<C-e>", "<C-\\><C-n>", {});
+
+-- Ctrl+s saves file
 map("", "<C-s>", ":write<CR>", {silent=true});
+
+-- Toggles file tree
 map("", "<C-b>", ":NERDTreeToggle<CR>", {silent=true});
-map("n", "<C-m>", ":Make<CR>", {silent=true});
-map("", "<C-_>", "<leader>c<space>", {silent=true});
-map("", "<leader>/", ":nohl<CR>", {silent=true});
+
+-- Toggle comment
+map("", "<leader>/", "<leader>c<space>", {silent=true});
+
+-- Esc removes highlight
+map("n", "<Esc>", ":nohl<CR>", {silent=true});
+
+-- Shift+M starts Markdown Preview
+map("n", "<S-m>", ":MarkdownPreview<CR>", {silent=true, noremap=true});
 
 -- Window navigation
 map("", "<leader>h", ":wincmd h<CR>", {silent=true, noremap=true});
@@ -17,25 +32,53 @@ map("", "<leader>j", ":wincmd j<CR>", {silent=true, noremap=true});
 map("", "<leader>k", ":wincmd k<CR>", {silent=true, noremap=true});
 map("", "<leader>l", ":wincmd l<CR>", {silent=true, noremap=true});
 
--- Autocomplete
-map("i", "<TAB>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], {noremap=true, expr=true});
-map("i", "<s-TAB>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], {noremap=true, expr=true});
-
-map("n", "<S-m>", ":MarkdownPreview<CR>", {silent=true, noremap=true});
-
 -- Fugitive
 map("n", "<leader>gs", ":G<CR>", {}); -- git status
 map("n", "<leader>gdt", ":diffget //2<CR>", {});
 map("n", "<leader>gdm", ":diffget //3<CR>", {});
 
 -- Telescope
-map("n", "<leader>tg", [[:lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<CR>]], {noremap=true});
-map("n", "<leader>tf", [[:lua require('telescope.builtin').find_files()<CR>]], {noremap=true});
+map("n", "<leader>fw", ":Telescope live_grep<CR>", {});
+map("n", "<leader>ff", ":Telescope find_files<CR>", {});
+map("n", "<leader>fb", ":Telescope buffers<CR>", {});
+map("n", "<leader>ft", ":Telescope help_tags<CR>", {});
+map("n", "<leader>fgs", ":Telescope git_status<CR>", {});
+map("n", "<leader>fgc", ":Telescope git_commits<CR>", {});
 
 -- Compe
+local function t(str)
+	return vim.api.nvim_replace_termcodes(str, true, true, true);
+end
+
+_G.tab_complete = function ()
+	if vim.fn.pumvisible() == 1 then
+		return t "<C-n>";
+	else
+		return t "<Tab>";
+	end
+end
+
+_G.s_tab_complete = function ()
+	if vim.fn.pumvisible() == 1 then
+		return t "<C-p>";
+	else
+		return t "<S-Tab>";
+	end
+end
+
+_G.cr_complete = function ()
+	return vim.fn["compe#confirm"]( {keys = t "<Plug>" .. "delimitMateCR", mode = ""});
+end
+
+map("i", "<Tab>", "v:lua.tab_complete()", {noremap=true, expr=true});
+map("s", "<Tab>", "v:lua.tab_complete()", {noremap=true, expr=true});
+map("i", "<S-Tab>", "v:lua.s_tab_complete()", {noremap=true, expr=true});
+map("s", "<S-Tab>", "v:lua.s_tab_complete()", {noremap=true, expr=true});
+
+map("i", "<CR>", "v:lua.cr_complete()", {noremap=true, expr=true});
+map("i", "<C-j>", "v:lua.cr_complete()", {noremap=true, expr=true});
+
 vimp.inoremap({"silent", "expr"}, "<C-space>", "compe#complete()");
-vim.cmd([[inoremap <silent><expr> <CR>      compe#confirm({ 'keys': "\<Plug>delimitMateCR", 'mode': '' })]])
-vim.cmd([[inoremap <silent><expr> <C-j>      compe#confirm({ 'keys': "\<Plug>delimitMateCR", 'mode': '' })]])
 vimp.inoremap({"silent", "expr"}, "<C-e>", "compe#close('<C-e>')");
 vimp.inoremap({"silent", "expr"}, "<C-f>", "compe#scroll({ 'delta': +4 })");
 vimp.inoremap({"silent", "expr"}, "<C-d>", "compe#scroll({ 'delta': -4 })");
