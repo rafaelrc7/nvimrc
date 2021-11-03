4e34d20c3c0f62461cf8d20e19418044aaa4ff51
rafaelrc7 <rafaelrc7@gmail.com>
2021-11-03 11:19:55 -0300

    Added custom settings for haskell

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,6 +3,10 @@ local utils = require("utils");
 local autocmds = {
 	trim_white_space = {
 		{"BufWritePre", "*", [[:%s/\s\+$//e]]}
+	},
+
+	haskell_spaces = {
+		{"FileType", "haskell", [[:setlocal shiftwidth=4 softtabstop=4 tabstop=8 expandtab]]}
 	}
 }
 
