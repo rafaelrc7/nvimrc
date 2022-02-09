116bbd3c94cfdc2413bcbe7b0b55480ddb8649b8
rafaelrc7 <rafaelrc7@gmail.com>
2022-02-09 01:09:10 -0300

    Changed default vimtex viewer

--- lua/plugin/vimtex.lua
+++ lua/plugin/vimtex.lua
@@ -1,5 +1,5 @@
 vim.g.tex_flavor = "latex";
 
-vim.g.vimtex_view_general_viewer = "qpdfview";
+vim.g.vimtex_view_general_viewer = "okular";
 vim.g.vimtex_view_general_options = [[--unique @pdf\#src:@tex:@line:@col]];
 
