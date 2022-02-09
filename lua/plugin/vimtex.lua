f458f9f8c9788f18b1000d31ce292b635aed10db
rafaelrc7 <rafaelrc7@gmail.com>
2022-02-09 00:57:39 -0300

    Removed deprecated option

--- lua/plugin/vimtex.lua
+++ lua/plugin/vimtex.lua
@@ -2,5 +2,4 @@ vim.g.tex_flavor = "latex";
 
 vim.g.vimtex_view_general_viewer = "qpdfview";
 vim.g.vimtex_view_general_options = [[--unique @pdf\#src:@tex:@line:@col]];
-vim.g.vimtex_view_general_options_latexmk = "--unique";
 
