16615f7f0b033e0b6ffcd8ed7e5d32ec15061a16
rafaelrc7 <rafaelrc7@gmail.com>
2022-03-21 20:35:35 -0300

    Enabled syntax with treesitter (for spelling)

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -15,6 +15,7 @@ require'nvim-treesitter.configs'.setup {
 	highlight = {
 		enable = true,  -- false will disable the whole extension
 		disable = { },  -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
 	},
 }
 
