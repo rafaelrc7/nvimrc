31a891c17366424ac96c83a50a8356d356f0ee9f
rafaelrc7 <rafaelrc7@gmail.com>
2022-03-28 16:55:03 -0300

    Updated nvim-tree config

--- lua/plugin/nvim-tree.lua
+++ lua/plugin/nvim-tree.lua
@@ -1,15 +1,29 @@
 local g = vim.g;
+local utils = require "utils";
 
-g.nvim_tree_quit_on_open = 1;
 g.nvim_tree_highlight_opened_files = 1;
+
+utils.nvim_create_augroups(
+	{
+		nvim_tree = {
+			-- autoclose
+			{"BufEnter", "*", "++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"},
+
+		},
+	}
+);
 
 require "nvim-tree".setup {
 	disable_netrw = true,
 	hijack_netrw = true;
-	auto_close = true,
 	open_on_tab = true,
 	filters = {
 		custom = {".git", "node_modules", ".cache"},
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
 	},
 };
 
