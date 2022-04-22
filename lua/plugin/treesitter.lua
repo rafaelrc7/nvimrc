ece0dedbdabb3a104f118bdead505c7972f0cebe
rafaelrc7 <rafaelrc7@gmail.com>
2022-04-22 02:17:07 -0300

    treesitter config change

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -10,13 +10,13 @@ parser_configs.norg = {
 }
 
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = "all",  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	ignore_install = { }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true,  -- false will disable the whole extension
-		disable = { },  -- list of language that will be disabled
 		additional_vim_regex_highlighting = true,
 	},
+	indent = {
+		enable = true,
+	}
 }
 
 vim.o.foldmethod = "expr";
