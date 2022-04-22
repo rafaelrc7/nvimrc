8e432710feff1201c5616ffc57d0ea5e6d0b7b67
rafaelrc7 <rafaelrc7@gmail.com>
2022-04-22 00:38:17 -0300

    Added module prefix

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,9 +1,12 @@
 local load_nvim_module = require"utils".load_nvim_module;
 
+local module_name = "plugin"
+
 local plugins = { "airline", "lsp", "neorg", "nvim-tree", "presence",
 				  "snippets", "telescope", "treesitter", "vimtex" }
 
+local module_path = module_name .. "."
 for _, plugin in ipairs(plugins) do
-	load_nvim_module(plugin)
+	load_nvim_module(module_path .. plugin)
 end
 
