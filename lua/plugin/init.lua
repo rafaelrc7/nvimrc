1287c1fa6c2690b9ea937e0bd80a7a5caf8492ce
rafaelrc7 <rafaelrc7@gmail.com>
2022-04-27 00:32:50 -0300

    Load cmp config

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -3,8 +3,8 @@
 local load_nvim_module = require"utils".load_nvim_module;
 
 local module_name = "plugin"
-local plugins = { "airline", "lsp", "neorg", "nvim-tree", "presence",
-				  "snippets", "telescope", "treesitter", "vimtex" }
+local plugins = { "airline", "cmp", "lsp", "neorg", "nvim-tree", "presence",
+	"snippets", "telescope", "treesitter", "vimtex" }
 
 local module_path = module_name .. "."
 for _, plugin in ipairs(plugins) do
